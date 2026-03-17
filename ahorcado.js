88a40c89cf5549d7ab77dc53e225fb40d1e48c06
VictorGuaicurba <guaicurbavictor04@gmail.com>
2026-03-17 17:51:22 -0400

    Test-Solo-Letras
    Se le implementó la herramienta del test /^[a-z]+$/ que verifica que solo haya letras de la 'a' a la 'z' denegando la introducción de números o signos al juego

---
+++ ahorcado.js
@@ -0,0 +1,89 @@
+const esperar = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+// Variables
+let palabraSecreta = "";
+let letrasAdivinadas = [];
+let intentos = 6;
+
+// Inicio del juego
+async function configurarJuego() {
+    console.log("Bienvenidos al Juego del Ahorcado");
+
+    let palabraValida = false;
+    while (!palabraValida) {
+        palabraSecreta = prompt("Bienvenidos, ingresa la palabra: ").toLowerCase();
+
+        // El test /^[a-z]+$/ verifica que solo haya letras de la 'a' a la 'z'
+        if (/^[a-zñáéíóú]+$/.test(palabraSecreta)) {
+            palabraValida = true;
+        }
+    }
+    
+    // Limpieza de pantalla
+    console.clear(); 
+
+    // Lista de progreso con los guiones
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        letrasAdivinadas.push("_");
+    }
+
+    console.log(`La palabra tiene ${palabraSecreta.length} letras.`);
+    console.log(`Tienes ${intentos} oportunidades.`);
+}
+
+// Los intentos
+function procesarIntento(letra) {
+    let acierto = false;
+
+    for (let i = 0; i < palabraSecreta.length; i++) {
+        if (palabraSecreta[i] === letra) {
+            letrasAdivinadas[i] = letra;
+            acierto = true;
+        }
+    }
+
+    if (acierto) {
+        let faltantes = 0;
+
+        for (let l of letrasAdivinadas) {
+            if (l === "_") faltantes++;
+        }
+        console.log(`¡Palabra correcta! Te faltan ${faltantes} letras.`);
+    } else {
+        intentos = intentos - 1;
+        console.log(`Letra incorrecta. Pierdes una oportunidad.`);
+        console.log(`Te quedan: ${intentos} intentos.`);
+    }
+}
+
+// El bucle del juego
+async function jugar() {
+    configurarJuego();
+
+    while (intentos > 0 && letrasAdivinadas.includes("_")) {
+        console.log("Estado actual: " + letrasAdivinadas.join(" "));
+        let letraUsuario = prompt("¿Qué letra desea poner?: ").toLowerCase();
+
+        // Validación
+        if (letraUsuario.length === 1 && /^[a-zñáéíóú]$/.test(letraUsuario)) {
+            procesarIntento(letraUsuario);
+
+            await esperar(500);
+            console.clear()
+        } else {
+            console.log("Por favor, ingresa solo una letra a la vez.");
+            
+            await esperar(500);
+            console.clear()
+        }
+    }
+
+    // Final para victoria o derrota
+    if (!letrasAdivinadas.includes("_")) {
+        console.log(`¡GANASTE! La palabra era: ${palabraSecreta}`);
+    } else {
+        console.log(`PERDISTE. Te has quedado sin vidas. La palabra era: ${palabraSecreta}`);
+    }
+}
+
+// Ejecución
+jugar();
